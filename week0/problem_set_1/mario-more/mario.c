e06e20cc92131bcb895c0a82ad4d37ade657fe7f
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-16 14:11:34 +0000

    Wed, Aug 16, 2023, 4:11 PM +02:00

--- week0/problem_set_1/mario-more/mario.c
+++ week0/problem_set_1/mario-more/mario.c
@@ -9,18 +9,20 @@ int get_input(void)
 
 void generate_output(int height)
 {
-    string output;
-    for (int row = 1, row < height, row++)
+    for (row = 1, row < height, row++)
     {
+        string output;
         for (int chr = 1, chr < height, chr++)
         {
-            output[chr] = row < 
+            output[chr] = (chr < row) ? "#" : " ";
         }
+        printf("%s", output);
     }
 }
 
 int main(void)
 {
-    printf("-> %d\n", get_input());
-
+    int height;
+    height = get_input();
+    generate_output(height);
 }
