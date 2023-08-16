c367ec1b0e91a184efb5c13c8fb067126768f401
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-16 14:37:56 +0000

    Wed, Aug 16, 2023, 4:37 PM +02:00

--- week0/problem_set_1/mario-more/mario.c
+++ week0/problem_set_1/mario-more/mario.c
@@ -7,22 +7,38 @@ int get_input(void)
     return n;
 }
 
+void generate_left_at_index(int row, int height)
+{
+    char output[height];
+    int chr;
+
+    for (chr = 0; chr < height; chr++)
+    {
+        output[chr] = ((chr >= row) && (chr >= height)) ? '#' : ' ';
+    }
+    printf("%s\n", output);
+}
+
 void generate_output(int height)
 {
-    int row, chr;
-    for (row = 0; row < height; row++)
+    // int row, chr;
+    // for (row = 0; row < height; row++)
+    // {
+    //     char output[height];
+    //     chr = 0;
+
+    //     while (chr <= height)
+    //     {
+    //        output[chr] = (chr > row) ? '0' : '#';
+    //        chr++;
+    //     }
+    //     output[++chr] = ' ';
+
+    // }
+    int row;
+    for (row=0; row<height; row++)
     {
-        char output[height];
-        chr = 0;
-
-        while (chr <= height)
-        {
-           output[chr] = (chr > row) ? '0' : '#';
-           chr++;
-        }
-        output[++chr] = ' ';
-
-        printf("%s\n", output);
+        generate_left_at_index(row, height);
     }
 }
 
