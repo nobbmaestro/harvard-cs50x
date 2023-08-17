94e2e505521ebf4682d9f4a387c02fbf215ae580
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-17 14:20:05 +0000

    Thu, Aug 17, 2023, 4:20 PM +02:00

--- week0/problem_set_1/credit/credit.c
+++ week0/problem_set_1/credit/credit.c
@@ -42,8 +42,9 @@ int get_first_digits(int *p_arr_num, int n_digits)
 
     for (int i = 0; i < n_digits; i++)
     {
-        int scale = pow10(digits-i);
+        int scale = pow(10, n_digits-i);
         digits = digits + (p_arr_num[i]*scale);
+        printf("%d %d\n", digits, scale);
     }
 
     return digits;
