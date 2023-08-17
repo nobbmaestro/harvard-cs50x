876eb891fc8cd3e23a1b959e7edeabb64399d1b3
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-17 11:37:54 +0000

    Thu, Aug 17, 2023, 1:37 PM +02:00

--- week0/problem_set_1/mario-more/mario.c
+++ week0/problem_set_1/mario-more/mario.c
@@ -20,13 +20,15 @@ void generate_left_at_index(int row, int height)
     output[++i] = ' ';
     for (j = i; j > 0; j--)
     {
-        output[i] = output[j];
+        output[i++] = output[j];
         i++;
-        printf("i = %d, output[i] = %d --- j = %d, output[j] = %d", i, j, output[i], output[j]);
+        // printf("i = %d, output[i] = %d --- j = %d, output[j] = %d\n", i, j, output[i], output[j]);
+        printf("i = %d, j = %d\n", i, j);
+        printf("output[i] = %c, output[j] = %c\n", output[i], output[j]);
     }
 
 
-    printf("%s\n", output);
+    printf("--> %s\n", output);
 }
 
 void generate_output(int height)
