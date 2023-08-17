b90de3f4b22829fa3b6fff7fe6b7d3b736859c21
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-17 12:10:12 +0000

    Thu, Aug 17, 2023, 2:10 PM +02:00

--- week0/problem_set_1/credit/credit.c
+++ week0/problem_set_1/credit/credit.c
@@ -1,7 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int get_card_number(void)
 {
-    
+    int num = 0;
+    while (num <= 0)
+    {
+        num = get_int("Number: ");
+    }
+    return num;
+}
+
+bool validate_card_number(void)
+{
+    /* Do nothing */
+    return false;
+}
+
+int main(void)
+{
+    int num;
+    num = get_card_number();
 }
