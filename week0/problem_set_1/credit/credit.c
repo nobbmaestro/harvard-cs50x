0e816d34804cd28131aae617d684889410c700f0
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-17 13:57:47 +0000

    Thu, Aug 17, 2023, 3:57 PM +02:00

--- week0/problem_set_1/credit/credit.c
+++ week0/problem_set_1/credit/credit.c
@@ -49,7 +49,7 @@ void convert_int_into_array(int *p_arr_num, unsigned int num)
     /* Append null termination at last used index */
     p_arr_num[len] = NULL_TERMINATION;
 
-    #if 0
+    #if 1
     for (i = 0; i < MAX_CARD_NUMBER_LEN; i++)
     {
         if (p_arr_num[i] != NULL_TERMINATION)
