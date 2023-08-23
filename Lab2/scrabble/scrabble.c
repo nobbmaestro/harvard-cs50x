57618ec8f760ebcedf405722414bcef4e167f471
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-23 19:30:29 +0000

    Wed, Aug 23, 2023, 9:30 PM +02:00

--- Lab2/scrabble/scrabble.c
+++ Lab2/scrabble/scrabble.c
@@ -48,6 +48,7 @@ int compute_score(string word)
             sum += POINTS[index];
             sum++;
         }
+        printf("letter: %c, value: %d, points: %d, sum: %d\n", value, index, POINTS[index], sum);
     }
     return sum;
 }
