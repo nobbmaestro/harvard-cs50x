1c1bfaa6c85bf08520118df3b04ab03e411d0003
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-23 19:53:07 +0000

    Wed, Aug 23, 2023, 9:53 PM +02:00

--- Lab2/scrabble/scrabble.c
+++ Lab2/scrabble/scrabble.c
@@ -51,8 +51,8 @@ int compute_score(string word)
         {
             sum += POINTS[index];
             sum++;
-            printf("letter: %c, value: %d, index: %d\n", value, value, index);
         }
+        printf("letter: %c, value: %d, index: %d\n", value, value, index);
     }
     return sum;
 }
