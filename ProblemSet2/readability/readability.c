736ada1deb7c69eb0b8ba4401375776aff814b0c
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-25 13:21:12 +0000

    Fri, Aug 25, 2023, 3:21 PM +02:00

--- ProblemSet2/readability/readability.c
+++ ProblemSet2/readability/readability.c
@@ -187,7 +187,7 @@ int grade_text(string text)
 
     avg_letters = calculate_avg_num_letters_per_words(letters, words);
     avg_sentences = calculate_avg_num_sentences_per_words(sentences, words);
-    grade = calculate_coleman_liau_index(avg_letters), round(avg_sentences));
+    grade = calculate_coleman_liau_index(avg_letters, avg_sentences);
 
 #if 0
     printf("\n");
