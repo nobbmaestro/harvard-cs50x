ec6ee207f0203ba7be35ff23cbd8364bb03431c2
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-25 13:52:54 +0000

    Fri, Aug 25, 2023, 3:52 PM +02:00

--- ProblemSet2/substitution/substitution.c
+++ ProblemSet2/substitution/substitution.c
@@ -8,16 +8,18 @@
 /* Main function */
 int main(int argc, string argv[])
 {
+    bool valid_key;
     string key, plain, cipher;
 
+    printf("argc: %d\n", argc);
 }
 
 /* Helpers */
 bool validate_key(string key)
 {
-    bool valid;
+    bool valid = false;
 
-    valid = strlen(key) != KEY_LEN;
+    // valid = strlen(key) != KEY_LEN;
 
     return valid;
 }
