13d3802d132387954c3f08e59d7d38c63244af2d
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-25 13:50:11 +0000

    Fri, Aug 25, 2023, 3:50 PM +02:00

--- ProblemSet2/substitution/substitution.c
+++ ProblemSet2/substitution/substitution.c
@@ -1,12 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+#define KEY_LEN 26
+
 /* Function prototypes */
 
 /* Main function */
 int main(int argc, string argv[])
 {
+    string key, plain, cipher;
 
 }
 
 /* Helpers */
+bool validate_key(string key)
+{
+    bool valid;
+
+    valid = strlen(key) != KEY_LEN;
+
+    return valid;
+}
