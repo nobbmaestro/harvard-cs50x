03e8a158e9791fb170d2dfd21cf5cd63428c3ca8
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-30 13:53:34 +0000

    Wed, Aug 30, 2023, 3:53 PM +02:00

--- ProblemSet3/tideman/tideman.c
+++ ProblemSet3/tideman/tideman.c
@@ -116,10 +116,10 @@ void record_preferences(int ranks[])
 {
     for (int i = 1; i < candidate_count; i++)
     {
-        rank = ranks[i];
+        int rank = ranks[i];
         for (int j = 1; j < candidate_count; j++)
         {
-            if (i != j)
+            if (rank != j)
             {
                 preferences[rank][j]++;
             }
