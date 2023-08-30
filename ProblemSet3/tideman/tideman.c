d459af7f4a8670ded1bfe9b2a7361b29dfaf6a3d
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-30 14:19:15 +0000

    Wed, Aug 30, 2023, 4:19 PM +02:00

--- ProblemSet3/tideman/tideman.c
+++ ProblemSet3/tideman/tideman.c
@@ -120,19 +120,19 @@ void record_preferences(int ranks[])
         {
             if (i != j)
             {
-                printf("%s > %s\n", candidates[i], candidates[j]);
-                preferences[i][j]++;
+                // printf("%s > %s\n", candidates[ranks[i]], candidates[ranks[j]]);
+                preferences[ranks[i]][ranks[j]]++;
             }
         }
     }
 
-    // for (int j = 0; j < candidate_count; j++)
-    // {
-    //     for (int k = 0; k < candidate_count; k++)
-    //     {
-    //         printf("%s > %s: %d\n", candidates[j], candidates[k], preferences[j][k]);
-    //     }
-    // }
+    for (int j = 0; j < candidate_count; j++)
+    {
+        for (int k = 0; k < candidate_count; k++)
+        {
+            printf("%s > %s: %d\n", candidates[j], candidates[k], preferences[j][k]);
+        }
+    }
 }
 
 // Record pairs of candidates where one is preferred over the other
