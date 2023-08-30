a116bc97f40622628c907d59e7ab80ecaba0e291
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-08-30 16:50:49 +0000

    Wed, Aug 30, 2023, 6:50 PM +02:00

--- ProblemSet3/tideman/tideman.c
+++ ProblemSet3/tideman/tideman.c
@@ -205,13 +205,13 @@ void lock_pairs(void)
         }
     }
 
-    for (int i = 0; i < candidate_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            printf("[%d][%d]: %d\n", i, j, locked[i][j]);
-        }
-    }
+    // for (int i = 0; i < candidate_count; i++)
+    // {
+    //     for (int j = 0; j < candidate_count; j++)
+    //     {
+    //         printf("[%d][%d]: %d\n", i, j, locked[i][j]);
+    //     }
+    // }
 }
 
 // Print the winner of the election
