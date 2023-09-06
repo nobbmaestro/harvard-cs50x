840447c910309d5be3e86d878d5e42e58e158ee4
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-06 11:09:46 +0000

    Wed, Sep 6, 2023, 1:09 PM +02:00

--- Lab4/smiley/helpers.c
+++ Lab4/smiley/helpers.c
@@ -6,18 +6,18 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    for (int h = 0; h < height; h++)
+    for (int i = 0; i < height; i++)
     {
-        for (int w = 0; w < width; w++)
+        for (int j = 0; j < width; j++)
         {
-            if (image[h][w] == 0x0)
-            {
-                image[h][w] = rgbtBlue;
-            }
-            else
-            {
-                /* Do nothing */
-            }
+            // if (image[h][w] == 0x0)
+            // {
+            //     image[h][w] = rgbtBlue;
+            // }
+            // else
+            // {
+            //     /* Do nothing */
+            // }
         }
     }
 }
