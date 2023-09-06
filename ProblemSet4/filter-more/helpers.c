d34d52041a97d18a0ff46af6017e093b931a4335
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-06 16:01:05 +0000

    Wed, Sep 6, 2023, 6:01 PM +02:00

--- ProblemSet4/filter-more/helpers.c
+++ ProblemSet4/filter-more/helpers.c
@@ -8,10 +8,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int avg = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
-            image[i][j].rgbtBlue = avg;
-            image[i][j].rgbtGreen = avg;
-            image[i][j].rgbtRed = avg;
+            int avg = ceil((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            if (avg > 0x0)
+            {
+                image[i][j].rgbtBlue = avg;
+                image[i][j].rgbtGreen = avg;
+                image[i][j].rgbtRed = avg;
+            }
         }
     }
 }
