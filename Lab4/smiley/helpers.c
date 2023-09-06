d0642c45554ceff3d1369c6ae0db1510603b1f0d
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-06 11:19:32 +0000

    Wed, Sep 6, 2023, 1:19 PM +02:00

--- Lab4/smiley/helpers.c
+++ Lab4/smiley/helpers.c
@@ -1,5 +1,4 @@
 #include "helpers.h"
-#include "bmp.h"
 
 #define RGBTb 0x00
 #define RGBTg 0xE8
@@ -7,19 +6,28 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE tmp;
+    tmp.rgbtBlue  = 0x0;
+    tmp.rgbtGreen = 0x0;
+    tmp.rgbtRed   = 0x0;
+
     // Change all black pixels to a color of your choosing
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // if (image[h][w] == 0x0)
-            // {
-            //     image[h][w] = rgbtBlue;
-            // }
-            // else
-            // {
-            //     /* Do nothing */
-            // }
+            if ((image[i][j].rgbtBlue == tmp.rgbtBlue) &&
+                (image[i][j].rgbtGreen == tmp.rgbtGreen) &&
+                (image[i][j].rgbtRed == tmp.rgbtRed))
+            {
+                image[i][j].rgbtBlue = RGBTb;
+                image[i][j].rgbtGreen = RGBTg;
+                image[i][j].rgbtRed = RGBTr;
+            }
+            else
+            {
+                /* Do nothing */
+            }
         }
     }
 }
