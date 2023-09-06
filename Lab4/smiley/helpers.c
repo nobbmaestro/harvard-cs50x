864f9eb0abde958b105a17c300ac2840c7968e62
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-06 11:01:18 +0000

    Wed, Sep 6, 2023, 1:01 PM +02:00

--- Lab4/smiley/helpers.c
+++ Lab4/smiley/helpers.c
@@ -1,6 +1,15 @@
 #include "helpers.h"
 
+#define DEFAULT_COLOR_HEX 0x00E8C6
+
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            if 
+        }
+    }
 }
