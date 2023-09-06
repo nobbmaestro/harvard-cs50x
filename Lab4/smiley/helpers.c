628f7a5267902c6d7619be3a716c29511b90a856
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-06 11:25:11 +0000

    Wed, Sep 6, 2023, 1:25 PM +02:00

--- Lab4/smiley/helpers.c
+++ Lab4/smiley/helpers.c
@@ -6,23 +6,15 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE tmp;
-
-    tmp.rgbtBlue  = 0x0;
-    tmp.rgbtGreen = 0x0;
-    tmp.rgbtRed   = 0x0;
-
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            if ((image[i][j].rgbtBlue == tmp.rgbtBlue) &&
-                (image[i][j].rgbtGreen == tmp.rgbtGreen) &&
-                (image[i][j].rgbtRed == tmp.rgbtRed))
+            if ((image[i][j].rgbtBlue == 0x00) && (image[i][j].rgbtGreen == 0x00) && (image[i][j].rgbtRed == 0x00))
             {
-                image[i][j].rgbtBlue = RGBTb;
+                image[i][j].rgbtBlue  = RGBTb;
                 image[i][j].rgbtGreen = RGBTg;
-                image[i][j].rgbtRed = RGBTr;
+                image[i][j].rgbtRed   = RGBTr;
             }
             else
             {
