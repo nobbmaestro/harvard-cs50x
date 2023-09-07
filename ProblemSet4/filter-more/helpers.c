7a16d89a2273e4f403f3ac4317a4e0f90cfa93ff
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-07 08:35:36 +0000

    Thu, Sep 7, 2023, 10:35 AM +02:00

--- ProblemSet4/filter-more/helpers.c
+++ ProblemSet4/filter-more/helpers.c
@@ -42,13 +42,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             if (i < mid)
             {
                 RGBTRIPLE tmp;
-                tmp.rgbtBlue  = image[height - i][width - j].rgbtBlue;
-                tmp.rgbtGreen = image[height - i][width - j].rgbtGreen;
-                tmp.rgbtRed   = image[height - i][width - j].rgbtRed;
+                tmp.rgbtBlue  = image[height - i][j].rgbtBlue;
+                tmp.rgbtGreen = image[height - i][j].rgbtGreen;
+                tmp.rgbtRed   = image[height - i][j].rgbtRed;
 
-                image[height - i][width - j].rgbtBlue  = image[i][j].rgbtBlue;
-                image[height - i][width - j].rgbtGreen = image[i][j].rgbtGreen;
-                image[height - i][width - j].rgbtRed   = image[i][j].rgbtRed;
+                image[height - i][j].rgbtBlue  = image[i][j].rgbtBlue;
+                image[height - i][j].rgbtGreen = image[i][j].rgbtGreen;
+                image[height - i][j].rgbtRed   = image[i][j].rgbtRed;
 
                 image[i][j].rgbtBlue  = tmp.rgbtBlue;
                 image[i][j].rgbtGreen = tmp.rgbtGreen;
