934c3f7437c61fd13a9fadc50c2df2160bc4eb72
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-07 08:08:22 +0000

    Thu, Sep 7, 2023, 10:08 AM +02:00

--- ProblemSet4/filter-more/helpers.c
+++ ProblemSet4/filter-more/helpers.c
@@ -32,6 +32,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGB
     return;
 }
 
