1b4fbff3d6c5c064a7c558e4d3b8b413d5a2bbe2
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-07 10:06:26 +0000

    Thu, Sep 7, 2023, 12:06 PM +02:00

--- ProblemSet4/filter-more/helpers.c
+++ ProblemSet4/filter-more/helpers.c
@@ -41,7 +41,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if (j <= mid)
+            if (j < mid)
             {
                 RGBTRIPLE tmp;
                 int k = width - i - 1;
