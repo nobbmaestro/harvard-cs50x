f64f3a5b5bd098a8b0688d2147e8370838081f0e
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-07 10:55:54 +0000

    Thu, Sep 7, 2023, 12:55 PM +02:00

--- ProblemSet4/filter-more/helpers.c
+++ ProblemSet4/filter-more/helpers.c
@@ -68,6 +68,8 @@ RGBTRIPLE get_neighbor_average(int index_h, int index_w, int height, int width, 
     int index_h_hi = (index_h < (height - 1)) ? (index_h + 1) : height;
     int index_w_hi = (index_w < (width - 1)) ? (index_w + 1) : width;
 
+    printf("avg at (%d, %d) -> from: (%d, %d), to: (%d, %d)\n", index_h, index_w, index_h_lo, index_w_lo, index_h_hi, index_w_hi);
+
     int elements = 0;
     for (int i = index_h_lo; i < index_h_hi; i++)
     {
