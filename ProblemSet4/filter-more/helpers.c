c015dfa731d853d0ee227877944e2ae661e86c0c
Norbert Batiuk <77891635+nobbmaestro@users.noreply.github.com>
2023-09-09 19:03:46 +0000

    Sat, Sep 9, 2023, 9:03 PM +02:00

--- ProblemSet4/filter-more/helpers.c
+++ ProblemSet4/filter-more/helpers.c
@@ -99,7 +99,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE weighted_sum = calculate_weighted_sum(i, j, height, width, tmp);
+            image[i][j] = calculate_weighted_sum(i, j, height, width, tmp);
         }
     }
     /* Free memory for image copy */
