0917d52a6a9695ce71a77021e0fc1354dc5c4c97
Norbert Batiuk <norbert@batiuk.se>
2023-10-10 16:27:06 +0200

    Add problem_set_7 movies

---
+++ 7_sql/problem_set_7/movies/5.sql
@@ -0,0 +1,17 @@
+/* Problem set 7: Movies, task 5
+
+Write a SQL query to list the titles and release years of all Harry Potter movies, in chronological order.
+- Your query should output a table with two columns, one for the title of each movie and one for the release year of 
+each movie.
+- You may assume that the title of all Harry Potter movies will begin with the words “Harry Potter”, and that if a 
+movie title begins with the words “Harry Potter”, it is a Harry Potter movie.
+*/
+SELECT
+    title,
+    YEAR
+FROM
+    movies
+WHERE
+    title LIKE "Harry Potter%"
+ORDER BY
+    YEAR ASC;
